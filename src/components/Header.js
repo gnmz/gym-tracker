5699ae04579a8279449141e40d722ba28a2c78e4
gnmx <gnum1x@yandex.ru>
2020-11-10 16:58:16 +0300

    change route header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        {window.location.href === 'http://localhost:3000/' ?  <h1 className="header__title">Gym's Tracker</h1> :
+        {window.location.href === '/' ?  <h1 className="header__title">Gym's Tracker</h1> :
         <Link to="/main">
           <h1 className="header__title">Gym's Tracker</h1>
         </Link>
