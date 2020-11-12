49d0c52cfddd8954a1d349917d94ecc016573b38
gnmx <gnum1x@yandex.ru>
2020-11-13 01:45:05 +0300

    change header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,14 @@ class Header extends Component {
   render() {
     return (
       <div className="header">
-        {window.location.href === 'https://gyms-tracker.herokuapp.com/' ? <Link to="/"> <h1 className="header__title">Gym's Tracker</h1> </Link> :
-        <Link to="/main">
-          <h1 className="header__title">Gym's Tracker</h1>
-        </Link>
+        {window.localStorage.length !== 0 ?
+          <Link to="/main">
+             <h1 className="header__title">Gym's Tracker</h1>
+          </Link>
+         :
+          <Link to="/">
+            <h1 className="header__title">Gym's Tracker</h1>
+          </Link>
         }
       </div>
     );
